f36b1fe9ae5881913f0aa72e5dac40b50807fd79
FerGarDi <fergardi@gmail.com>
2015-03-27 14:35:58 +0100

    Added background and styles.

--- src/Archmage/GameBundle/Controller/HomeController.php
+++ src/Archmage/GameBundle/Controller/HomeController.php
@@ -10,6 +10,7 @@ class HomeController extends Controller
 {
     /**
      * @Route("/")
+     * @Route("/home")
      * @Template("ArchmageGameBundle:Home:index.html.twig")
      */
     public function indexAction()

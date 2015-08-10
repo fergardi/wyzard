efb36afde664c34c1051a9561302782b06ff0f97
Fergardi <fergardi@gmail.com>
2015-08-10 22:25:51 +0200

    Arreglado un error al overridear la funcion format de TwigExtension, cambiada a nf de numberformat.

--- src/Archmage/GameBundle/Twig/TwigExtension.php
+++ src/Archmage/GameBundle/Twig/TwigExtension.php
@@ -7,11 +7,11 @@ class TwigExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            new \Twig_SimpleFilter('format', array($this, 'format')),
+            new \Twig_SimpleFilter('nf', array($this, 'nf')),
         );
     }
 
-    public function format($number, $decimals = 0, $decPoint = ',', $thousandsSep = '.')
+    public function nf($number, $decimals = 0, $decPoint = ',', $thousandsSep = '.')
     {
         $price = number_format((float)$number, $decimals, $decPoint, $thousandsSep);
         return $price;

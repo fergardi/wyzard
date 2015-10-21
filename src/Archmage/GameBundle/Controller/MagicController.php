29c625e94d251fb14cb9af379f692498eb12a6c4
Fernando <fernando@Fernando.intracampus.unileon.es>
2015-10-21 15:09:23 +0200

    Bug en una llave.

--- src/Archmage/GameBundle/Controller/MagicController.php
+++ src/Archmage/GameBundle/Controller/MagicController.php
@@ -415,7 +415,6 @@ class MagicController extends Controller
             $chance = rand(0,99);
             if ($chance < $maxchance) {
                 $artifacts = $manager->getRepository('ArchmageGameBundle:Artifact')->findAll();
-                $number = $spell->getSkill()->getArtifactBonus() * $player->getMagic();
                 shuffle($artifacts);
                 $artifact = $artifacts[0]; //suponemos length > 0
                 $item = $player->hasArtifact($artifact);
@@ -429,7 +428,7 @@ class MagicController extends Controller
                     $item->setPlayer($player);
                     $player->addItem($item);
                 }
-                $this->addFlash('success', 'Has encontrado el artefacto <span class="label label-' . $item->getArtifact()->getFaction()->getClass() . '"><a href="' . $this->generateUrl('archmage_game_home_help') . '#' . $this->get('service.controller')->toSlug($item->getArtifact()->getName()) . '" class="link">' . $item->getArtifact()->getName() . '</a></span>.');}
+                $this->addFlash('success', 'Has encontrado el artefacto <span class="label label-' . $item->getArtifact()->getFaction()->getClass() . '"><a href="' . $this->generateUrl('archmage_game_home_help') . '#' . $this->get('service.controller')->toSlug($item->getArtifact()->getName()) . '" class="link">' . $item->getArtifact()->getName() . '</a></span>.');
             } else {
                 $this->addFlash('danger', 'No has encontrado nada.');
             }
